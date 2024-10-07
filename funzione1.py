862400be0c295d6a723d0c3508a54582d5b83d8f
verlanti2002 <verlanti2002@gmail.com>
2024-10-07 17:22:41 +0200

    modify appunti_complementi.txt

--- funzione1.py
+++ funzione1.py
@@ -1,16 +1,19 @@
-# Scrivere un programma che chieda all’utente di immettere una serie di valori terminata dal simbolo '#'
+# Scrivere un programma che chieda all'utente di immettere una serie di valori terminata dal simbolo '#'
 # Dopodichè applica una funzione a vostro piacere su tutti gli elementi
-# Non usare un ciclo per l’applicazione della funzione
+# Non usare un ciclo per l'applicazione della funzione
 
 def square(n):
     return n**2
 
 lista = []
 while True:
-    val = input('Inserire un valore (# per terminare): ')
-    if val != '#':
-        lista.append(int(val))
-    else:
-        break
+    try:
+        val = input('Inserire un valore (# per terminare): ')
+        if val != '#':
+            lista.append(int(val))
+        else:
+            break
+    except ValueError as e:
+        print(f'Exception: {e}')
 
 print(list(map(square, lista)))
